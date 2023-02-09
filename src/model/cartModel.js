2442a36d07a0e92d6132f5a1b50f6e34f16292f1
vinti-roy <vintiray71@gmail.com>
2023-02-09 16:12:36 +0530

    done

--- src/model/cartModel.js
+++ src/model/cartModel.js
@@ -34,15 +34,3 @@ const cartSchema = new mongoose.Schema({
 
 module.exports=mongoose.model("cart",cartSchema)
 
-//{
-//     userId: {ObjectId, refs to User, mandatory, unique},
-//     items: [{
-//       productId: {ObjectId, refs to Product model, mandatory},
-//       quantity: {number, mandatory, min 1}
-//     }],
-//     totalPrice: {number, mandatory, comment: "Holds total price of all the items in the cart"},
-//     totalItems: {number, mandatory, comment: "Holds total number of items in the cart"},
-//     createdAt: {timestamp},
-//     updatedAt: {timestamp},
-//   }
-//   ```
